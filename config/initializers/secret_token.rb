e8f9a76cb5b79ceb6224d9c92e29a92308cda46e
Nicholas Sandberg <nss118@gmail.com>
2013-09-08 20:55:08 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleCMS::Application.config.secret_token = '475d1f2c3481269f5fa6a7bd92ddc91fb036e90fe1aa9c0501e64f7e869268d0057b1a140319e4a67f42896a86f4d013a436e41e9677c1835a4d9dfbcae489c8'
