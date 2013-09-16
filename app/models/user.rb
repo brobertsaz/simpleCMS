2059153d0efef78ad94aed80422f4abd71aee2c4
Nicholas Sandberg <nss118@gmail.com>
2013-09-15 20:26:14 -0700

    user login created

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :first_name, :last_name, :password, :password_confirmation
   has_secure_password
   before_save { |user| user.email = email.downcase }
+  before_save :create_remember_token
   validates_confirmation_of :password
   validates :password, presence: true
   validates :password_confirmation, presence: true
@@ -23,4 +24,10 @@ class User < ActiveRecord::Base
   validates :last_name, presence: true
   has_many :posts
   has_many :comments
+
+  private
+
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
