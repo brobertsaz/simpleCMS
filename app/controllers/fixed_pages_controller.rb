f224634975bb3b0325b041217297ec191b9f155d
Nicholas Sandberg <nss118@gmail.com>
2013-09-17 19:03:21 -0700

    a little style

--- app/controllers/fixed_pages_controller.rb
+++ app/controllers/fixed_pages_controller.rb
@@ -1,5 +1,9 @@
 class FixedPagesController < ApplicationController
   def home
+    if signed_in?
+      user = User.find(current_user.id)
+      redirect_to user_pages_show_path(:user_id => current_user)
+    end
   end
 
   def logout
